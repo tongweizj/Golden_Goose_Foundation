59ee3560562b8cf68d9a2279f9675f70bd8431e2
max <max@luci.ai>
2020-12-04 16:17:31 -0500

    update

--- api/app/config/db.config.js
+++ api/app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb://express-demo:express-demo@192.168.0.100:27017/express-api-demo",
+  url: "mongodb://ggfadmin:12345678@192.168.0.100:27017/ggf",
   testUrl: "mongodb://admin:123456@174.138.38.105:1008/db_jds"
 };
