5eb507229cb19879b06b4e7435023ec667f44c43
max <max@luci.ai>
2020-12-09 11:20:12 -0500

    fixbug: web没有显示最近 1 个月的涨幅数据

--- web/models/fund-increase.js
+++ web/models/fund-increase.js
@@ -8,6 +8,7 @@ const fundIncreaseSchema = new Schema({
   unitNetWorth:String,
   dayOfGrowth:String,
   recent1Week:String,
+  recent1Month:String,
   recent3Month:String,
   recent6Month:String,
   recent1Year:String,
